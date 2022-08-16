ec5a661168403850ba6a6e49c26ac1f4278c5541
Gabipretel <gabypretel@gmail.com>
2022-08-15 22:01:21 -0300

    clinic-animals-v1.5

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 function Paciente() {
   return (
-    <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
-          <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre:{' '}
+    <div className='mx-3 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre:{' '}
             <span className='font-normal normal-case'>Benji</span>
-          </p>
+        </p>
           
-          <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario:{' '}
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario:{' '}
             <span className='font-normal normal-case'>Gabriel</span>
-          </p>
+        </p>
 
-          <p className='font-bold mb-3 text-gray-700 uppercase'>Email:{' '}
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Email:{' '}
             <span className='font-normal normal-case'>gabypretel@gmail.com</span>
-          </p>
+        </p>
 
-          <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de alta:{' '}
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de alta:{' '}
             <span className='font-normal normal-case'>10 de Diciembre</span>
-          </p>
+        </p>
 
-          <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas:{' '}
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas:{' '}
             <span className='font-normal normal-case'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam minus sit deleniti quae voluptates! Optio nam aut, facilis placeat repellendus perferendis architecto neque doloremque laudantium voluptatum. Ullam architecto officiis accusamus.</span>
-          </p>
+        </p>
 
       </div>
   )
