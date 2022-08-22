4c39b787bab2dcdc8b98fe78b93b20f56acc9777
Gabipretel <gabypretel@gmail.com>
2022-08-22 00:12:18 -0300

    actualizacion

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 
 function Paciente({paciente}) {
+    const {nombre,propietario,email,alta,sintomas}= paciente;
   return (
     <div className='mx-3 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
         <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre:{' '}
-            <span className='font-normal normal-case'>{paciente.nombre}</span>
+            <span className='font-normal normal-case'>{nombre}</span>
         </p>
-          
+        
         <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario:{' '}
-            <span className='font-normal normal-case'>{paciente.propietario}</span>
+            <span className='font-normal normal-case'>{propietario}</span>
         </p>
 
         <p className='font-bold mb-3 text-gray-700 uppercase'>Email:{' '}
-            <span className='font-normal normal-case'>{paciente.email}</span>
+            <span className='font-normal normal-case'>{email}</span>
         </p>
 
         <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de alta:{' '}
-            <span className='font-normal normal-case'>{paciente.alta}</span>
+            <span className='font-normal normal-case'>{alta}</span>
         </p>
 
         <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas:{' '}
-            <span className='font-normal normal-case'>{paciente.sintomas}</span>
+            <span className='font-normal normal-case'>{sintomas}</span>
         </p>
 
       </div>
